9d49d1ac4087303ec687b73d7b45d40443273ee2
IndiV74 <porutenko@gmail.com>
2020-04-21 12:15:03 +0300

    debug Config Vars

--- EPBot.py
+++ EPBot.py
@@ -43,8 +43,7 @@ def load_dictonary_from_GoogleDrive(file_id, fileName):
         SERVICE_ACCOUNT_INFO['type'] = os.environ['type']
         SERVICE_ACCOUNT_INFO['project_id'] = os.environ['project_id']
         SERVICE_ACCOUNT_INFO['private_key_id'] = os.environ['private_key_id']
-        private_key = os.environ['private_key']
-        SERVICE_ACCOUNT_INFO['private_key'] = private_key
+        SERVICE_ACCOUNT_INFO['private_key'] = os.environ['private_key'].replace('\\\\', '\\')
         SERVICE_ACCOUNT_INFO['client_email'] = os.environ['client_email']
         SERVICE_ACCOUNT_INFO['client_id'] = os.environ['client_id']
         SERVICE_ACCOUNT_INFO['auth_uri'] = os.environ['auth_uri']
