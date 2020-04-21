c0180f507abb9730faa503f26f223117d7cc49f2
IndiV74 <porutenko@gmail.com>
2020-04-21 15:17:26 +0300

    debug Config Vars

--- EPBot.py
+++ EPBot.py
@@ -52,7 +52,7 @@ def load_dictonary_from_GoogleDrive(file_id, fileName):
         SERVICE_ACCOUNT_INFO['client_x509_cert_url'] = os.environ['client_x509_cert_url']
 
         with open('epbot-274622-530f5ad65b26.json', 'w', encoding='utf-8') as json_file:
-            json.dump(SERVICE_ACCOUNT_INFO, SERVICE_ACCOUNT_INFO, ensure_ascii=False)
+            json.dump(SERVICE_ACCOUNT_INFO, json_file, ensure_ascii=False)
 
         with open('epbot-274622-530f5ad65b26.json', 'r', encoding='utf-8') as fSERVICE_ACCOUNT_INFO:  # открываем файл на чтение
             SERVICE_ACCOUNT_INFO = json.load(fSERVICE_ACCOUNT_INFO)
