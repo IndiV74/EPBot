d2b84933679bc8f92a8a07910438384752313d14
IndiV74 <porutenko@gmail.com>
2020-04-21 17:08:37 +0300

    debug Config Vars

--- EPBot.py
+++ EPBot.py
@@ -52,7 +52,7 @@ def load_dictonary_from_GoogleDrive(file_id, fileName):
         SERVICE_ACCOUNT_INFO['token_uri'] = os.environ['token_uri']
         SERVICE_ACCOUNT_INFO['auth_provider_x509_cert_url'] = os.environ['auth_provider_x509_cert_url']
         SERVICE_ACCOUNT_INFO['client_x509_cert_url'] = os.environ['client_x509_cert_url']
-        print(SERVICE_ACCOUNT_INFO)
+
 
     #     with open('epbot-274622-530f5ad65b26.json', 'w', encoding='utf-8') as json_file:
     #         json.dump(SERVICE_ACCOUNT_INFO, json_file, ensure_ascii=False)
@@ -66,6 +66,7 @@ def load_dictonary_from_GoogleDrive(file_id, fileName):
     #
     # print(SERVICE_ACCOUNT_INFO)
     # print(SERVICE_ACCOUNT_INFO['private_key'])
+    print(SERVICE_ACCOUNT_INFO)
     credentials = service_account.Credentials.from_service_account_info(
         SERVICE_ACCOUNT_INFO, scopes=SCOPES)
     service = build('drive', 'v3', credentials=credentials)
